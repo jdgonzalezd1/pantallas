e4414406b720e585bc0ee3d32af49c2ed0cba9a0
jdgonzalezd1 <60698448+jdgonzalezd1@users.noreply.github.com>
2023-07-09 11:17:46 -0500

    Pantalla Facultad lista

--- src/pages/fac.js
+++ src/pages/fac.js
@@ -1,16 +1,25 @@
 import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { Worker } from '@react-pdf-viewer/core';
 import { Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import { getFilePlugin } from '@react-pdf-viewer/get-file';
-import { useFetchGet } from './fetchData';
+import { setRequest } from './loadData';
 
 function Fac() {
     const getFilePluginInstance = getFilePlugin();
     const { Download } = getFilePluginInstance;
     const [facultad, setFacultad] = useState([]);
     const [statusF, setStatusF] = useState([]);
-    
+
+    const [pdf, setPdf] = useState([]);
+    const [pdfUrl, setPdfUrl] = useState("");
+
+    const [objt, setObjt] = useState({});
+    const [userId, setUserId] = useState("1000689373");
+    const location = useLocation();
+    const { reportId } = location.state;
+
     const fetchFacultadData = async () => {
         try {
             const result = await fetch("http://localhost:8081/filtro/facultad");
@@ -20,15 +29,41 @@ function Fac() {
             console.log("Error", error);
         }
     }
-    
+
+    const fetchPdfData = async () => {
+        try {
+            setObjt({
+                dato: statusF,
+                reporte: reportId,
+                usuario: userId
+            })
+            const result = await fetch("http://localhost:8081/report/generar", {
+                method: "POST",
+
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(objt)
+            });
+            const parsedResponse = await result.json();
+            setPdf(parsedResponse);
+            let url = setRequest(pdf);
+            setPdfUrl(url);
+        } catch (error) {
+            console.log("Error xd", error);
+        }
+
+    }
+
     useEffect(() => {
         fetchFacultadData();
     }, []);
 
     return <>
         <div hidden>
-            <input id='reportId' type='text'></input>
-            <input id='userId' type='text'></input>
+            <input id='reportId' value={reportId} type='text'></input>
+            <input id='userId' type='text' value={userId}></input>
         </div>
         <div className="flex-container">
             <div>
@@ -44,14 +79,16 @@ function Fac() {
                 </select>
             </div>
             <div>
-                <button type="button">Generar reporte</button>
+                <button type="button" onClick={fetchPdfData}>Generar reporte</button>
             </div>
 
         </div>
         <div>
             <div className="pdf-section">
                 <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js'>
-                    <Viewer fileUrl="http://localhost:8081/archivo/get/reporte/RepGIsFac-FACULTAD DE INGENIERIA-1000689373.pdf" plugins={[getFilePluginInstance]} />
+                    {pdfUrl && (
+                        <Viewer fileUrl={pdfUrl} plugins={[getFilePluginInstance]} />
+                    )}
                 </Worker>
             </div>
         </div>
